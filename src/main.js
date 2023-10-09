4303bfc4009874197767c53ddd9e7b9d7e0de922
Idle <651462+reonZ@users.noreply.github.com>
2023-10-09 14:29:19 +0200

    sort settings & add client header

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { MODULE_ID, warn } from './module'
+import { MODULE_ID, localize, warn } from './module'
 import { registerArp } from './modules/arp'
 import { registerEffectsPanelHelper } from './modules/effects'
 import { registerGiveth } from './modules/giveth'
@@ -18,34 +18,50 @@ const MODULES = [
     registerEffectsPanelHelper(),
     registerSpellsSummary(),
 ]
+
 const CONFLICTS = new Set()
+
+let firstClientSetting = null
 
 Hooks.once('init', () => {
     const user = game.data.users.find(x => x._id === game.data.userId)
     const isGM = user && user.role >= CONST.USER_ROLES.GAMEMASTER
 
-    for (const module of MODULES) {
-        const { settings = [], init, conflicts = [] } = module
-
-        for (const setting of settings) {
-            const name = setting.name
+    const settings = MODULES.flatMap(({ settings = [] }) =>
+        settings.map(setting => {
+            const key = setting.name
 
             if (setting.choices) {
                 setting.choices = setting.choices.reduce((choices, choice) => {
-                    choices[choice] = settingPath(name, `choices.${choice}`)
+                    choices[choice] = settingPath(key, `choices.${choice}`)
                     return choices
                 }, {})
             }
 
-            game.settings.register(MODULE_ID, name, {
-                scope: 'world',
-                config: true,
-                ...setting,
-                name: settingPath(name, 'name'),
-                hint: settingPath(name, 'hint'),
-            })
-        }
+            setting.key = key
+            setting.scope ??= 'world'
+            setting.config ??= true
+            setting.name = settingPath(key, 'name')
+            setting.hint = settingPath(key, 'hint')
 
+            return setting
+        })
+    )
+
+    const [worldSettings, clientSettings] = ['world', 'client'].map(scope =>
+        settings.filter(settings => settings.scope === scope)
+    )
+
+    ;[worldSettings, clientSettings].forEach(settings =>
+        settings.forEach(setting => game.settings.register(MODULE_ID, setting.key, setting))
+    )
+
+    if (isGM) {
+        firstClientSetting = clientSettings[0].key
+        Hooks.on('renderSettingsConfig', renderSettingsConfig)
+    }
+
+    MODULES.forEach(({ init, conflicts = [] }) => {
         if (isGM) {
             for (const id of conflicts) {
                 const conflictingModule = game.modules.get(id)
@@ -57,7 +73,7 @@ Hooks.once('init', () => {
         }
 
         if (!module.conflicting && init) init(isGM)
-    }
+    })
 })
 
 Hooks.once('ready', () => {
@@ -76,4 +92,28 @@ Hooks.once('ready', () => {
 
 function settingPath(setting, key) {
     return `${MODULE_ID}.settings.${setting}.${key}`
+}
+
+function renderSettingsConfig(_, html) {
+    if (!firstClientSetting) return
+
+    const group = html.find(`.tab[data-tab=${MODULE_ID}] [data-setting-id="${MODULE_ID}.${firstClientSetting}"]`)
+    group.before(`<h3>${localize('settings.client')}</h3>`)
+
+    // function beforeGroup(name, key, dom = 'h3') {
+    //     const localized = localize(`menu.${key}`)
+    //     tab.find(`[name="${MODULE_ID}.${name}"]`).closest('.form-group').before(`<${dom}>${localized}</${dom}>`)
+    // }
+
+    // if (game.user.isGM) {
+    //     beforeGroup('enabled', 'client.header', 'h2')
+    // }
+
+    // beforeGroup('saves', 'client.tooltip')
+    // beforeGroup('distance', 'client.distance')
+    // beforeGroup('height', 'client.sidebar')
+    // beforeGroup('actions', 'client.actions')
+    // beforeGroup('containers', 'client.items')
+    // beforeGroup('spells', 'client.spells')
+    // beforeGroup('untrained', 'client.skills')
 }
