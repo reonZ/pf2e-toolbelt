5235ec95c6db8956bf5e9836289b5051d669261c
Idle <651462+reonZ@users.noreply.github.com>
2023-11-14 02:59:20 +0100

    fixed badgeless conditions

--- src/macros/condition.js
+++ src/macros/condition.js
@@ -6,14 +6,15 @@ const localize = subLocalize('macros.condition')
 export async function permaConditionEffect(actor) {
     const callback = (html, type) => {
         const condition = html.find('[name=condition]')
+        const { name, slug, img } = condition.find(':selected').data()
+
         return {
             type,
-            name:
-                html.find('[name=name]').val().trim() ||
-                localize('effect-name', { condition: condition.find(':selected').data().name }),
+            slug,
+            img,
+            name: html.find('[name=name]').val().trim() || localize('effect-name', { condition: name }),
             uuid: condition.val(),
             badge: Number(html.find('[name=badge]').val() || 1),
-            img: condition.find(':selected').data().img,
             unidentified: html.find('[name=unidentified]').prop('checked'),
         }
     }
@@ -31,16 +32,22 @@ export async function permaConditionEffect(actor) {
         },
     }
 
+    const conditions = Array.from(game.pf2e.ConditionManager.conditions.values())
+    const withBadge = new Set(conditions.filter(condition => !!condition.badge).map(condition => condition.slug))
+
     const content = await renderTemplate(templatePath('macros/condition'), {
         i18n: localize,
-        conditions: Array.from(
-            new Set(Array.from(game.pf2e.ConditionManager.conditions.values()).sort((a, b) => a.name.localeCompare(b.name)))
-        ),
+        conditions: Array.from(new Set(conditions.sort((a, b) => a.name.localeCompare(b.name)))),
     })
 
-    const setName = html => {
-        const condition = html.find('[name=condition] :selected').data().name
-        html.find('[name=name]').attr('placeholder', localize('effect-name', { condition }))
+    const setInputs = html => {
+        const { name, slug } = html.find('[name=condition] :selected').data()
+        html.find('[name=name]').prop('placeholder', localize('effect-name', { condition: name }))
+
+        const hasBadge = withBadge.has(slug)
+        const badge = html.find('[name=badge]')
+        badge.prop('disabled', !hasBadge)
+        if (!hasBadge) badge.val(1)
     }
 
     const result = await Dialog.wait(
@@ -50,8 +57,8 @@ export async function permaConditionEffect(actor) {
             title: localize('title'),
             close: () => null,
             render: html => {
-                setName(html)
-                html.find('[name=condition]').on('change', () => setName(html))
+                setInputs(html)
+                html.find('[name=condition]').on('change', () => setInputs(html))
             },
         },
         {
@@ -68,7 +75,7 @@ export async function permaConditionEffect(actor) {
         uuid: result.uuid,
     }
 
-    if (result.badge > 1) {
+    if (result.badge > 1 && withBadge.has(result.slug)) {
         rule.alterations = [
             {
                 mode: 'override',
