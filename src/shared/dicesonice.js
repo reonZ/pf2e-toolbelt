57080614a81b81add5f150dbd29cdcba5c95d2b3
Idle <651462+reonZ@users.noreply.github.com>
2024-01-24 14:33:22 +0100

    simplified

--- src/shared/dicesonice.js
+++ src/shared/dicesonice.js
@@ -1,10 +1,7 @@
-export async function roll3dDice(roll, user = game.user, synchronize = true){
-    // if DSN is active and doesn't display chat card immediately, delay until the roll is done
-    if(game.modules.get('dice-so-nice')?.active){
-        let dsnRoll = game.dice3d.showForRoll(roll, user, synchronize);
-        if(!game.settings.get("dice-so-nice", "immediatelyDisplayChatMessages")){
-            return dsnRoll;
-        } 
-    }
-    return Promise.resolve(true);
+export async function roll3dDice(
+	roll,
+	{ user = game.user, synchronize = true } = {},
+) {
+	if (!game.modules.get("dice-so-nice")?.active) return;
+	return game.dice3d.showForRoll(roll, user, synchronize);
 }
