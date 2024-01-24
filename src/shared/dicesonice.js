878a92650179f684882e069bbac770eb3668e2a0
kromko <34245546+Cuingamehtar@users.noreply.github.com>
2024-01-24 14:15:25 +0300

    Actually show the dice to other players

--- src/shared/dicesonice.js
+++ src/shared/dicesonice.js
@@ -1,7 +1,7 @@
-export async function roll3dDice(roll){
+export async function roll3dDice(roll, user = game.user, synchronize = true){
     // if DSN is active and doesn't display chat card immediately, delay until the roll is done
     if(game.modules.get('dice-so-nice')?.active){
-        let dsnRoll = game.dice3d.showForRoll(roll);
+        let dsnRoll = game.dice3d.showForRoll(roll, user, synchronize);
         if(!game.settings.get("dice-so-nice", "immediatelyDisplayChatMessages")){
             return dsnRoll;
         } 
