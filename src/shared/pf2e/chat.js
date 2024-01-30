e8bc5e0fbd3551ca15e74f5f871e4f6e1ac0ea2f
Idle <651462+reonZ@users.noreply.github.com>
2024-01-30 15:15:34 +0100

    update chat helpers

--- src/shared/pf2e/chat.js
+++ src/shared/pf2e/chat.js
@@ -20,8 +20,15 @@ export async function applyDamageFromMessage(
 		rollIndex = 0,
 	},
 ) {
-	if (promptModifier)
-		return shiftAdjustDamage(token, { message, multiplier, rollIndex });
+	if (promptModifier) return shiftAdjustDamage(message, multiplier, rollIndex);
+
+	const tokens = [token];
+	if (tokens.length === 0) {
+		ui.notifications.error("PF2E.ErrorMessage.NoTokenSelected", {
+			localize: true,
+		});
+		return;
+	}
 
 	const shieldBlockRequest = CONFIG.PF2E.chatDamageButtonShieldToggle;
 	const roll = message.rolls.at(rollIndex);
@@ -40,122 +47,122 @@ export async function applyDamageFromMessage(
 		.map((o) => o.replace(/^self/, "origin"));
 	const messageItem = message.item;
 
-	if (!token.actor) return;
+	for (const token of tokens) {
+		if (!token.actor) continue;
 
-	// If no target was acquired during a roll, set roll options for it during damage application
-	if (!messageRollOptions.some((o) => o.startsWith("target"))) {
-		messageRollOptions.push(...token.actor.getSelfRollOptions("target"));
-	}
-	const domain = multiplier > 0 ? "damage-received" : "healing-received";
-	const ephemeralEffects =
-		multiplier > 0
-			? await extractEphemeralEffects({
-					affects: "target",
-					origin: message.actor,
-					target: token.actor,
-					item: message.item,
-					domains: [domain],
-					options: messageRollOptions,
-			  })
-			: [];
-	const contextClone = token.actor.getContextualClone(
-		originRollOptions,
-		ephemeralEffects,
-	);
-	const applicationRollOptions = new Set([
-		...messageRollOptions.filter((o) => !/^(?:self|target):/.test(o)),
-		...originRollOptions,
-		...contextClone.getSelfRollOptions(),
-	]);
+		// If no target was acquired during a roll, set roll options for it during damage application
+		if (!messageRollOptions.some((o) => o.startsWith("target"))) {
+			messageRollOptions.push(...token.actor.getSelfRollOptions("target"));
+		}
+		const domain = multiplier > 0 ? "damage-received" : "healing-received";
+		const ephemeralEffects =
+			multiplier > 0
+				? await extractEphemeralEffects({
+						affects: "target",
+						origin: message.actor,
+						target: token.actor,
+						item: message.item,
+						domains: [domain],
+						options: messageRollOptions,
+				  })
+				: [];
+		const contextClone = token.actor.getContextualClone(
+			originRollOptions,
+			ephemeralEffects,
+		);
+		const applicationRollOptions = new Set([
+			...messageRollOptions.filter((o) => !/^(?:self|target):/.test(o)),
+			...originRollOptions,
+			...contextClone.getSelfRollOptions(),
+		]);
 
-	// Target-specific damage/healing adjustments
-	const outcome = message.flags.pf2e.context?.outcome;
-	const breakdown = [];
-	const rolls = [];
-	if (typeof damage === "number" && damage < 0) {
-		const critical = outcome === "criticalSuccess";
+		// Target-specific damage/healing adjustments
+		const outcome = message.flags.pf2e.context?.outcome;
+		const breakdown = [];
+		const rolls = [];
+		if (typeof damage === "number" && damage < 0) {
+			const critical = outcome === "criticalSuccess";
 
-		const resolvables = (() => {
-			if (messageItem?.isOfType("spell")) return { spell: messageItem };
-			if (messageItem?.isOfType("weapon")) return { weapon: messageItem };
-			return {};
-		})();
+			const resolvables = (() => {
+				if (messageItem?.isOfType("spell")) return { spell: messageItem };
+				if (messageItem?.isOfType("weapon")) return { weapon: messageItem };
+				return {};
+			})();
 
-		const damageDice = extractDamageDice(
-			contextClone.synthetics.damageDice,
-			[domain],
-			{
+			const damageDice = extractDamageDice(
+				contextClone.synthetics.damageDice,
+				[domain],
+				{
+					resolvables,
+					test: applicationRollOptions,
+				},
+			).filter(
+				(d) =>
+					(d.critical === null || d.critical === critical) &&
+					d.predicate.test(applicationRollOptions),
+			);
+
+			for (const dice of damageDice) {
+				const formula = `${dice.diceNumber}${dice.dieSize}[${dice.label}]`;
+				const roll = await new Roll(formula).evaluate({ async: true });
+				roll._formula = `${dice.diceNumber}${dice.dieSize}`; // remove the label from the main formula
+				await roll.toMessage({
+					flags: { pf2e: { suppressDamageButtons: true } },
+					flavor: dice.label,
+					speaker: ChatMessage.getSpeaker({ token }),
+				});
+				breakdown.push(`${dice.label} ${dice.diceNumber}${dice.dieSize}`);
+				rolls.push(roll);
+			}
+			if (rolls.length) {
+				damage -= rolls
+					.map((roll) => roll.total)
+					.reduce((previous, current) => previous + current);
+			}
+
+			const modifiers = extractModifiers(contextClone.synthetics, [domain], {
 				resolvables,
-				test: applicationRollOptions,
-			},
-		).filter(
-			(d) =>
-				(d.critical === null || d.critical === critical) &&
-				d.predicate.test(applicationRollOptions),
-		);
+			}).filter(
+				(m) =>
+					(m.critical === null || m.critical === critical) &&
+					m.predicate.test(applicationRollOptions),
+			);
 
-		for (const dice of damageDice) {
-			const formula = `${dice.diceNumber}${dice.dieSize}[${dice.label}]`;
-			const roll = await new Roll(formula).evaluate({ async: true });
-			roll._formula = `${dice.diceNumber}${dice.dieSize}`; // remove the label from the main formula
-			await roll.toMessage({
-				flags: { pf2e: { suppressDamageButtons: true } },
-				flavor: dice.label,
-				speaker: ChatMessage.getSpeaker({ token }),
-			});
-			breakdown.push(`${dice.label} ${dice.diceNumber}${dice.dieSize}`);
-			rolls.push(roll);
-		}
-		if (rolls.length) {
-			damage -= rolls
-				.map((roll) => roll.total)
-				.reduce((previous, current) => previous + current);
+			// unlikely to have any typed modifiers, but apply stacking rules just in case even though the context of
+			// previously applied modifiers has been lost
+			damage -= applyStackingRules(modifiers ?? []);
+
+			// target-specific modifiers breakdown
+			breakdown.push(
+				...modifiers
+					.filter((m) => m.enabled)
+					.map((m) => `${m.label} ${signedInteger(m.modifier)}`),
+			);
 		}
 
-		const modifiers = extractModifiers(contextClone.synthetics, [domain], {
-			resolvables,
-		}).filter(
-			(m) =>
-				(m.critical === null || m.critical === critical) &&
-				m.predicate.test(applicationRollOptions),
-		);
+		const hasDamageOrHealing =
+			typeof damage === "number" ? damage !== 0 : damage.total !== 0;
+		const notes = hasDamageOrHealing
+			? extractNotes(contextClone.synthetics.rollNotes, [domain]).filter(
+					(n) =>
+						(!outcome ||
+							n.outcome.length === 0 ||
+							n.outcome.includes(outcome)) &&
+						n.predicate.test(applicationRollOptions),
+			  )
+			: [];
 
-		// unlikely to have any typed modifiers, but apply stacking rules just in case even though the context of
-		// previously applied modifiers has been lost
-		damage -= applyStackingRules(modifiers ?? []);
-
-		// target-specific modifiers breakdown
-		breakdown.push(
-			...modifiers
-				.filter((m) => m.enabled)
-				.map((m) => `${m.label} ${signedInteger(m.modifier)}`),
-		);
+		await contextClone.applyDamage({
+			damage,
+			token,
+			item: message.item,
+			skipIWR: multiplier <= 0,
+			rollOptions: applicationRollOptions,
+			shieldBlockRequest,
+			breakdown,
+			notes,
+		});
 	}
-
-	const hasDamage =
-		typeof damage === "number" ? damage !== 0 : damage.total !== 0;
-	const notes = (() => {
-		if (!hasDamage) return [];
-		return extractNotes(contextClone.synthetics.rollNotes, [domain])
-			.filter(
-				(n) =>
-					(!outcome || n.outcome.length === 0 || n.outcome.includes(outcome)) &&
-					n.predicate.test(applicationRollOptions),
-			)
-			.map((note) => note.text);
-	})();
-
-	await contextClone.applyDamage({
-		damage,
-		token,
-		item: message.item,
-		skipIWR: multiplier <= 0,
-		rollOptions: applicationRollOptions,
-		shieldBlockRequest,
-		breakdown,
-		notes,
-	});
-
 	toggleOffShieldBlock(message.id);
 
 	/**
@@ -185,7 +192,7 @@ export function onClickShieldBlock(target, shieldButton, messageEl) {
 				animation: "fade",
 				trigger: "click",
 				arrow: false,
-				content: $(messageEl).find("div.hover-content"),
+				content: htmlQuery(messageEl, "div.hover-content"),
 				contentAsHTML: true,
 				contentCloning: true,
 				debug: false,
@@ -221,7 +228,7 @@ export function onClickShieldBlock(target, shieldButton, messageEl) {
 						!CONFIG.PF2E.chatDamageButtonShieldToggle;
 					return false;
 				},
-				functionFormat: (instance, _helper, $content) => {
+				functionFormat: (instance, _helper, contentEl) => {
 					const tokens = getTokens();
 					const nonBrokenShields = getNonBrokenShields(tokens);
 					const multipleShields =
@@ -231,12 +238,11 @@ export function onClickShieldBlock(target, shieldButton, messageEl) {
 
 					// If the actor is wielding more than one shield, have the user pick which shield to use for blocking.
 					if (multipleShields && !shieldActivated) {
-						const content = $content[0];
 						// Populate the list with the shield options
-						const listEl = htmlQuery(content, "ul.shield-options");
-						if (!listEl) return $content;
-						const shieldList = [];
-						for (const shield of nonBrokenShields) {
+						const listEl = htmlQuery(contentEl, "ul.shield-options");
+						if (!listEl) return $(contentEl);
+
+						const shieldList = nonBrokenShields.map((shield) => {
 							const input = document.createElement("input");
 							input.classList.add("data");
 							input.type = "radio";
@@ -256,14 +262,18 @@ export function onClickShieldBlock(target, shieldButton, messageEl) {
 							hardness.classList.add("tag");
 							const hardnessLabel = game.i18n.localize("PF2E.HardnessLabel");
 							hardness.innerHTML = `${hardnessLabel}: ${shield.hardness}`;
+
 							const itemLi = document.createElement("li");
 							itemLi.classList.add("item");
 							itemLi.append(input, shieldName, hardness);
-							shieldList.push(itemLi);
-						}
+
+							return itemLi;
+						});
+
 						listEl.replaceChildren(...shieldList);
 					}
-					return $content;
+
+					return $(contentEl);
 				},
 			})
 			.tooltipster("open");
