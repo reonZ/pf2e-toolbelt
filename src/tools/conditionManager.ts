51b3442c6fda31ff85d35950525b51b8501304d0
Idle <651462+reonZ@users.noreply.github.com>
2025-03-20 14:59:40 +0100

    add label to manager & set origin to current combatant

--- src/tools/conditionManager.ts
+++ src/tools/conditionManager.ts
@@ -7,6 +7,7 @@ import {
     ConditionPF2e,
     DurationData,
     EffectSource,
+    EncounterPF2e,
     GrantItemSource,
     ItemPF2e,
     R,
@@ -16,6 +17,7 @@ import {
 import { createTool } from "../tool";
 
 let MANAGE = false;
+let EFFECT_LABEL: string | undefined;
 
 const { config, settings, hook, localize, render } = createTool({
     name: "conditionManager",
@@ -53,8 +55,12 @@ const { config, settings, hook, localize, render } = createTool({
 } as const);
 
 class ConditionManager extends foundry.applications.api.ApplicationV2 {
+    #label: string = "";
     #actor: ActorPF2e;
+    #combat: EncounterPF2e | null;
+    #combatant: CombatantPF2e | null;
     #origin: CombatantPF2e | null;
+    #condition: ConditionPF2e;
     #counter: { value: number; default: number } | undefined;
     #rule: GrantItemSource & { alterations: any[] };
     #effect: PreCreate<EffectSource> & {
@@ -93,12 +99,16 @@ class ConditionManager extends foundry.applications.api.ApplicationV2 {
             });
         }
 
+        this.#condition = condition;
+        this.#combatant = this.#actor.combatant;
+        this.#combat = this.#combatant?.encounter ?? null;
+        this.#origin = this.#combat?.combatant ?? this.#combatant;
+
         this.#effect = {
             type: "effect",
-            name: `${game.i18n.localize("TYPES.Item.effect")}: ${condition.name}`,
+            name: this.effectLabel,
             img: condition.img,
             system: {
-                // tokenIcon: { show: false },
                 unidentified: false,
                 duration: {
                     expiry: "turn-start",
@@ -108,8 +118,6 @@ class ConditionManager extends foundry.applications.api.ApplicationV2 {
                 rules: [this.#rule],
             },
         };
-
-        this.#origin = this.#actor.combatant;
 
         if (condition.system.value.isValued) {
             this.#rule.inMemoryOnly = true;
@@ -125,24 +133,42 @@ class ConditionManager extends foundry.applications.api.ApplicationV2 {
         return this.#effect.system;
     }
 
+    get effectLabel(): string {
+        const base = (EFFECT_LABEL ??= game.i18n.localize("TYPES.Item.effect"));
+        const label = `${base}: ${this.#condition.name}`;
+
+        return this.#origin && this.#origin !== this.#combatant
+            ? `${label} (${this.#origin.name})`
+            : label;
+    }
+
     async close(options: ApplicationClosingOptions = {}): Promise<this> {
         options.animate = false;
         return super.close(options);
     }
 
     async _prepareContext(options: ApplicationRenderOptions): Promise<ConditionManagerContext> {
+        const combat = this.#combat;
         const combatants = R.pipe(
-            this.#origin?.encounter?.combatants.contents ?? [],
+            combat?.combatants.contents ?? [],
             R.filter((combatant) => !!combatant.actor),
             R.map((combatant) => {
                 return { value: combatant.id, label: combatant.name };
             })
         );
+        const origin = combatants.length ? { combatants, selected: this.#origin!.id } : null;
+
+        const labelPlaceholder = this.effectLabel;
+        const label = {
+            placeholder: labelPlaceholder,
+            value: this.#label || labelPlaceholder,
+        };
 
         return {
+            label,
+            origin,
             isGM: game.user.isGM,
             data: this.system,
-            origin: combatants.length ? { combatants, selected: this.#origin!.id } : null,
             timeUnits: CONFIG.PF2E.timeUnits,
             counter: this.#counter,
             expiryOptions: [
@@ -188,6 +214,8 @@ class ConditionManager extends foundry.applications.api.ApplicationV2 {
             };
         }
 
+        this.#effect.name = this.#label || this.effectLabel;
+
         this.#actor.createEmbeddedDocuments("Item", [this.#effect]);
     }
 
@@ -210,6 +238,11 @@ class ConditionManager extends foundry.applications.api.ApplicationV2 {
 
         addListenerAll(html, "[name]", "change", (event, el: HTMLInputElement) => {
             switch (el.name as EventChangeName) {
+                case "label": {
+                    this.#label = el.value.trim();
+                    break;
+                }
+
                 case "origin": {
                     this.#origin = this.#origin?.encounter?.combatants.get(el.value) ?? null;
                     break;
@@ -277,6 +310,7 @@ function onPreCreateItem(item: ItemPF2e<ActorPF2e>) {
 }
 
 type EventChangeName =
+    | "label"
     | "origin"
     | "system.badge.value"
     | "system.duration.unit"
@@ -295,6 +329,7 @@ type ConditionManagerContext = {
     origin: { combatants: { value: string; label: string }[]; selected: string } | null;
     timeUnits: typeof CONFIG.PF2E.timeUnits;
     counter: { value: number; default: number } | undefined;
+    label: { value: string; placeholder: string };
     expiryOptions: {
         value: string;
         label: string;
