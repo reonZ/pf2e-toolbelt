21303f783cfea494d356da10c958d5bc0882c51e
Idle <651462+reonZ@users.noreply.github.com>
2025-04-03 17:37:47 +0200

    implement spread teleport

--- src/tools/teleport.ts
+++ src/tools/teleport.ts
@@ -1,7 +1,37 @@
-import { TokenPF2e, positionTokenFromCoords } from "module-helpers";
+import { R, TokenPF2e, positionTokenFromCoords } from "module-helpers";
 import { createTool } from "../tool";
 
 const TOKENS: TokenPF2e[] = [];
+
+const innerSpread: PointSpread[] = [
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+];
+
+const outerSpread: PointSpread[] = [
+    [-2, -2],
+    [-1, -2],
+    [0, -2],
+    [1, -2],
+    [2, -2],
+    [2, -1],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+    [1, 2],
+    [0, 2],
+    [-1, 2],
+    [-2, 2],
+    [-2, 1],
+    [-2, 0],
+    [-2, -1],
+];
 
 const { config, settings } = createTool({
     name: "teleport",
@@ -10,6 +40,13 @@ const { config, settings } = createTool({
             key: "enabled",
             type: Boolean,
             default: false,
+            scope: "client",
+            gmOnly: true,
+        },
+        {
+            key: "spread",
+            type: Boolean,
+            default: true,
             scope: "client",
             gmOnly: true,
         },
@@ -59,13 +96,91 @@ function disableTeleport() {
 
 async function onCanvasStagePointerDown(event: PIXI.FederatedPointerEvent) {
     const operation = { animate: false, bypass: true };
-    const coords = event.getLocalPosition(canvas.app.stage);
-    const updates: { _id: string; x: number; y: number }[] = TOKENS.map((token) => {
-        const { x, y } = positionTokenFromCoords(coords, token);
-        return { _id: token.id, x, y };
-    });
+    const updates = settings.spread ? spreadTokens(event) : groupTokens(event);
 
     await canvas.scene?.updateEmbeddedDocuments("Token", updates, operation);
+    disableTeleport();
 }
+
+function spreadTokens(event: PIXI.FederatedPointerEvent): PositionUpdate[] {
+    const updates: PositionUpdate[] = [];
+
+    const [largeTokens, tokens] = R.partition(TOKENS, (token) => token.document.width > 1);
+    const hasLargeToken = largeTokens.length > 1;
+
+    const gridSize = canvas.grid.size;
+    const localPosition = event.getLocalPosition(canvas.app.stage);
+    const origin = canvas.grid.getSnappedPoint(localPosition, {
+        mode: CONST.GRID_SNAPPING_MODES.CENTER,
+    });
+
+    const spreads: { raw: PointSpread; coords?: Point | false }[] = (
+        hasLargeToken ? [...outerSpread, ...innerSpread] : [...innerSpread, ...outerSpread]
+    ).map((raw) => ({ raw }));
+
+    if (hasLargeToken) {
+        for (const token of largeTokens) {
+            const { x, y } = positionTokenFromCoords(origin, token);
+            updates.push({ _id: token.id, x, y });
+        }
+        spreads.push({ raw: [0, 0], coords: localPosition });
+    } else {
+        spreads.unshift({ raw: [0, 0], coords: localPosition });
+    }
+
+    let spreadIndex = -1;
+
+    const addUpdate = (token: TokenPF2e): boolean => {
+        spreadIndex = spreadIndex + 1 >= spreads.length ? 0 : spreadIndex + 1;
+        const spread = spreads[spreadIndex];
+
+        if (spread.coords === undefined) {
+            const offsetX = spread.raw[0] * gridSize;
+            const offsetY = spread.raw[1] * gridSize;
+
+            const target = {
+                x: origin.x + offsetX,
+                y: origin.y + offsetY,
+            };
+
+            const intersects = CONFIG.Canvas.polygonBackends.move.testCollision(origin, target, {
+                type: "move",
+                mode: "any",
+            });
+
+            spread.coords = intersects
+                ? false
+                : {
+                      x: localPosition.x + offsetX,
+                      y: localPosition.y + offsetY,
+                  };
+        }
+
+        if (!spread.coords) return false;
+
+        const { x, y } = positionTokenFromCoords(spread.coords, token);
+        updates.push({ _id: token.id, x, y });
+
+        return true;
+    };
+
+    for (const token of tokens) {
+        while (!addUpdate(token)) {}
+    }
+
+    return updates;
+}
+
+function groupTokens(event: PIXI.FederatedPointerEvent): PositionUpdate[] {
+    const position = event.getLocalPosition(canvas.app.stage);
+
+    return TOKENS.map((token) => {
+        const { x, y } = positionTokenFromCoords(position, token);
+        return { _id: token.id, x, y };
+    });
+}
+
+type PointSpread = [number, number];
+type PositionUpdate = { _id: string; x: number; y: number };
 
 export { config as teleportTool };
