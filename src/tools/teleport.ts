9e7098f674a3eb1cbf24bdcf3ef2ddea89440011
Idle <651462+reonZ@users.noreply.github.com>
2025-04-05 23:29:43 +0200

    rework the teleport tool

--- src/tools/teleport.ts
+++ src/tools/teleport.ts
@@ -1,9 +1,10 @@
-import { R, TokenPF2e, positionTokenFromCoords } from "module-helpers";
+import { MODULE, R, TokenPF2e, positionTokenFromCoords } from "module-helpers";
 import { createTool } from "../tool";
 
 const TOKENS: TokenPF2e[] = [];
 
-const innerSpread: PointSpread[] = [
+const SPREADS: SpreadPoint[] = [
+    [0, 0],
     [-1, -1],
     [0, -1],
     [1, -1],
@@ -12,9 +13,6 @@ const innerSpread: PointSpread[] = [
     [0, 1],
     [-1, 1],
     [-1, 0],
-];
-
-const outerSpread: PointSpread[] = [
     [-2, -2],
     [-1, -2],
     [0, -2],
@@ -33,11 +31,32 @@ const outerSpread: PointSpread[] = [
     [-2, -1],
 ];
 
+const INNER_ORTHOS: SpreadPoint[] = [
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+    [1, 0],
+];
+
+const OUTER_ORTHOS: SpreadPoint[] = [
+    [0, -2],
+    [0, 2],
+    [-2, 0],
+    [2, 0],
+];
+
 const { config, settings } = createTool({
     name: "teleport",
     settings: [
         {
             key: "enabled",
+            type: Boolean,
+            default: false,
+            scope: "client",
+            gmOnly: true,
+        },
+        {
+            key: "select",
             type: Boolean,
             default: false,
             scope: "client",
@@ -93,71 +112,135 @@ async function onCanvasStagePointerDown(event: PIXI.FederatedPointerEvent) {
 
     await canvas.scene?.updateEmbeddedDocuments("Token", updates, operation);
 
-    disableTeleport();
+    if (settings.select) {
+        canvas.tokens.releaseAll();
 
-    if (event.button !== 0) {
+        for (const token of TOKENS) {
+            token.control({ releaseOthers: false });
+        }
+    } else if (event.button !== 0) {
         canvas.tokens.releaseAll();
     }
+
+    disableTeleport();
 }
 
 function spreadTokens(event: PIXI.FederatedPointerEvent): PositionUpdate[] {
     const updates: PositionUpdate[] = [];
 
     const [largeTokens, tokens] = R.partition(TOKENS, (token) => token.document.width > 1);
-    const hasLargeToken = largeTokens.length > 1;
+    const hasLargeToken = largeTokens.length > 0;
 
     const gridSize = canvas.grid.size;
     const localPosition = event.getLocalPosition(canvas.app.stage);
-    const origin = canvas.grid.getSnappedPoint(localPosition, {
+    const clickPosition = canvas.grid.getSnappedPoint(localPosition, {
         mode: CONST.GRID_SNAPPING_MODES.CENTER,
     });
 
-    const spreads: { raw: PointSpread; coords?: Point | false }[] = (
-        hasLargeToken ? [...outerSpread, ...innerSpread] : [...innerSpread, ...outerSpread]
-    ).map((raw) => ({ raw }));
-
     if (hasLargeToken) {
-        for (const token of largeTokens) {
-            const { x, y } = positionTokenFromCoords(origin, token);
-            updates.push({ _id: token.id, x, y });
+        const sortedLargeTokens = R.sortBy(largeTokens, [(token) => token.document.width, "desc"]);
+
+        for (let i = 0; i < sortedLargeTokens.length; i++) {
+            const token = sortedLargeTokens[i];
+            const { x, y } = positionTokenFromCoords(clickPosition, token, false);
+
+            updates.push({ _id: token.id, x, y, sort: i });
         }
-        spreads.push({ raw: [0, 0], coords: localPosition });
-    } else {
-        spreads.unshift({ raw: [0, 0], coords: localPosition });
+    }
+
+    const getOffsetPosition = (spread: SpreadPoint): Point => {
+        return {
+            x: clickPosition.x + spread[0] * gridSize,
+            y: clickPosition.y + spread[1] * gridSize,
+        };
+    };
+
+    const testCollision = (
+        targetSpread: SpreadPoint,
+        originSpread: SpreadPoint = [0, 0]
+    ): boolean => {
+        const origin = getOffsetPosition(originSpread);
+        const target = getOffsetPosition(targetSpread);
+
+        const intersects = CONFIG.Canvas.polygonBackends.move.testCollision(origin, target, {
+            type: "move",
+            mode: "any",
+        });
+
+        if (!intersects && MODULE.isDebug) {
+            canvas.controls.debug
+                .lineStyle(4, 0x16a103)
+                .moveTo(origin.x, origin.y)
+                .lineTo(target.x, target.y);
+        }
+
+        return intersects;
+    };
+
+    const getSpreadKey = ([x, y]: SpreadPoint): SpreadKey => {
+        return `${x}x${y}`;
+    };
+
+    const origins: SpreadPoint[] = [[0, 0]];
+    const originKeys: SpreadKey[] = ["0x0"];
+
+    // we test for collision against ortho squares to later use them as origin too
+    for (let i = 0; i < OUTER_ORTHOS.length; i++) {
+        const outerSpread = OUTER_ORTHOS[i];
+        const innerSpread = INNER_ORTHOS[i];
+
+        if (!testCollision(outerSpread)) {
+            origins.push(outerSpread);
+            origins.push(innerSpread);
+            originKeys.push(getSpreadKey(outerSpread));
+            originKeys.push(getSpreadKey(innerSpread));
+        } else if (!testCollision(innerSpread)) {
+            origins.push(innerSpread);
+            originKeys.push(getSpreadKey(innerSpread));
+        }
     }
 
     let spreadIndex = -1;
 
+    const spreadMap: Record<SpreadKey, Point | false> = {};
+
     const addUpdate = (token: TokenPF2e): boolean => {
-        spreadIndex = spreadIndex + 1 >= spreads.length ? 0 : spreadIndex + 1;
-        const spread = spreads[spreadIndex];
+        spreadIndex = spreadIndex + 1 >= SPREADS.length ? 0 : spreadIndex + 1;
 
-        if (spread.coords === undefined) {
-            const offsetX = spread.raw[0] * gridSize;
-            const offsetY = spread.raw[1] * gridSize;
+        const spread = SPREADS[spreadIndex];
+        const spreadKey = getSpreadKey(spread);
 
-            const target = {
-                x: origin.x + offsetX,
-                y: origin.y + offsetY,
-            };
+        test: if (spreadMap[spreadKey] === undefined) {
+            if (originKeys.includes(spreadKey)) {
+                spreadMap[spreadKey] = getOffsetPosition(spread);
+                break test;
+            }
 
-            const intersects = CONFIG.Canvas.polygonBackends.move.testCollision(origin, target, {
-                type: "move",
-                mode: "any",
-            });
+            for (const origin of origins) {
+                if (!testCollision(spread, origin)) {
+                    spreadMap[spreadKey] = getOffsetPosition(spread);
+                    break test;
+                }
+            }
 
-            spread.coords = intersects
-                ? false
-                : {
-                      x: localPosition.x + offsetX,
-                      y: localPosition.y + offsetY,
-                  };
+            spreadMap[spreadKey] = false;
         }
 
-        if (!spread.coords) return false;
+        const coords = spreadMap[spreadKey];
+        if (!coords) return false;
 
-        const { x, y } = positionTokenFromCoords(spread.coords, token);
-        updates.push({ _id: token.id, x, y });
+        const { x, y } = positionTokenFromCoords(coords, token);
+        const update: PositionUpdate = { _id: token.id, x, y };
+
+        if (hasLargeToken) {
+            update.sort = largeTokens.length;
+        }
+
+        if (token.document.width < 1) {
+            update.sort = (update.sort ?? 0) + 1;
+        }
+
+        updates.push(update);
 
         return true;
     };
@@ -178,7 +261,14 @@ function groupTokens(event: PIXI.FederatedPointerEvent): PositionUpdate[] {
     });
 }
 
-type PointSpread = [number, number];
-type PositionUpdate = { _id: string; x: number; y: number };
+type SpreadPoint = [number, number];
+type SpreadKey = `${number}x${number}`;
+
+type PositionUpdate = {
+    _id: string;
+    x: number;
+    y: number;
+    sort?: number;
+};
 
 export { config as teleportTool };
