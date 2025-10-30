5d513c86302b48963de989d66ddca87308da57e8
Idle <651462+reonZ@users.noreply.github.com>
2025-10-30 15:19:01 +0100

    still nothing on the unscale department

--- src/tools/better-ui.ts
+++ src/tools/better-ui.ts
@@ -2,6 +2,7 @@ import {
     activateHooksAndWrappers,
     createToggleableHook,
     toggleHooksAndWrappers,
+    TokenPF2e,
 } from "module-helpers";
 import { ModuleTool, ToolSettingsList } from "module-tool";
 
@@ -54,7 +55,40 @@ class BetterUITool extends ModuleTool<ToolSettings> {
         const x = (width * scale - width) / 2 / scale;
         const y = (height * scale - height) / 2 / scale;
 
-        html.style.transform = `scale(${scale}) translate(-${x}px, -${y}px)`;
+        // for (const el of html.childNodes) {
+        //     if (!(el instanceof HTMLElement)) continue;
+        //     el.style.transform = `scale(${scale})`;
+        // }
+        // html.style.transform = `scale(${scale}) translate(-${x}px, -${y}px)`;
+        this.#test();
+    }
+
+    #test() {
+        const token = canvas.hud.token.object as TokenPF2e | undefined;
+        if (!token) return;
+
+        const gridSize = canvas.grid.size;
+        const scale = token.worldTransform.a;
+        const uiScale = canvas.dimensions.uiScale;
+        const transform = canvas.stage.worldTransform;
+        const bounds = token.bounds;
+
+        const worldLeft = transform.a * bounds.x + transform.c * bounds.y + transform.tx;
+        const worldTop = transform.b * bounds.x + transform.d * bounds.y + transform.ty;
+        const worldWidth = bounds.width * scale;
+        const worldHeight = bounds.height * scale;
+
+        const width = (worldWidth / uiScale) * (gridSize / 100);
+        const height = (worldHeight / uiScale) * (gridSize / 100);
+        const left = worldLeft + (worldWidth - width) / 2;
+        const top = worldTop + (worldHeight - height) / 2;
+
+        console.log({
+            width,
+            height,
+            left,
+            top,
+        });
     }
 }
 
