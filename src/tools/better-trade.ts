4f40e9c84f127a61d03ba0b7a15c028a04edec11
Idle <651462+reonZ@users.noreply.github.com>
2026-02-13 02:11:09 +0100

    add better-trade & giveth tools

---
+++ src/tools/better-trade.ts
@@ -0,0 +1,59 @@
+import { createCreatureSheetWrapper, DropCanvasItemData } from "foundry-helpers";
+import { CreaturePF2e, ItemPF2e } from "foundry-pf2e";
+import { ModuleTool, ToolSettingsList } from "module-tool";
+
+class BetterTradeTool extends ModuleTool<ToolSettings> {
+    #onDropItemWrapper = createCreatureSheetWrapper("WRAPPER", "_onDropItem", this.#creatureSheetOnDropItem, {
+        context: this,
+    });
+
+    get key(): "betterTrade" {
+        return "betterTrade";
+    }
+
+    get settingsSchema(): ToolSettingsList<ToolSettings> {
+        return [
+            {
+                key: "invertTrade",
+                type: Boolean,
+                default: false,
+                scope: "user",
+                playerOnly: true,
+                onChange: (value: boolean) => {
+                    if (!game.user.isGM) {
+                        this.#onDropItemWrapper.toggle(value);
+                    }
+                },
+            },
+        ];
+    }
+
+    ready(isGM: boolean): void {
+        if (!isGM && this.settings.invertTrade) {
+            this.#onDropItemWrapper.activate();
+        }
+    }
+
+    async #creatureSheetOnDropItem(
+        actor: CreaturePF2e,
+        wrapped: libWrapper.RegisterCallback,
+        event: DragEvent,
+        data: DropCanvasItemData & { fromInventory?: boolean },
+    ): Promise<ItemPF2e[]> {
+        if (!data.fromInventory || !data.uuid || fromUuidSync<ItemPF2e>(data.uuid)?.actor === actor) {
+            return wrapped(event, data);
+        }
+
+        const newEvent = new DragEvent("drop", {
+            altKey: event.altKey,
+            shiftKey: !event.shiftKey,
+            dataTransfer: event.dataTransfer,
+        });
+
+        return wrapped(newEvent, data);
+    }
+}
+
+type ToolSettings = toolbelt.Settings["betterTrade"];
+
+export { BetterTradeTool };
---
+++ src/tools/giveth.ts
@@ -0,0 +1,187 @@
+import {
+    createCreatureSheetWrapper,
+    createEmitable,
+    DropCanvasItemData,
+    isAllyActor,
+    isInstanceOf,
+    userIsGM,
+} from "foundry-helpers";
+import {
+    ActorPF2e,
+    ChoiceSetRuleElement,
+    ConditionPF2e,
+    ConditionSource,
+    CreaturePF2e,
+    CreatureSheetPF2e,
+    EffectPF2e,
+    EffectSource,
+    EffectTrait,
+    ItemPF2e,
+} from "foundry-pf2e";
+import { ModuleTool, ToolSettingsList } from "module-tool";
+
+const EFFECT_SETTING = ["disabled", "ally", "all"] as const;
+
+class GivethTool extends ModuleTool<ToolSettings> {
+    #givethEmitable = createEmitable(this.key, this.#givethEffect.bind(this));
+
+    #handleDroppedItemWrapper = createCreatureSheetWrapper(
+        "MIXED",
+        "_handleDroppedItem",
+        this.#creatureSheetHandleDroppedItem,
+        { context: this },
+    );
+
+    get key(): "giveth" {
+        return "giveth";
+    }
+
+    get settingsSchema(): ToolSettingsList<ToolSettings> {
+        return [
+            {
+                key: "effect",
+                type: String,
+                choices: EFFECT_SETTING,
+                default: "disabled",
+                scope: "world",
+                onChange: () => {
+                    this.configurate();
+                },
+            },
+        ];
+    }
+
+    get api(): Record<string, any> {
+        return {
+            canDropEffectOnActor: (item: ItemPF2e, actor: ActorPF2e): boolean => {
+                return this.#shouldHandleEffectDrop(item, actor);
+            },
+        };
+    }
+
+    ready(): void {
+        this._configurate();
+    }
+
+    _configurate(): void {
+        const effects = this.settings.effect !== "disabled";
+
+        if (userIsGM()) {
+            this.#givethEmitable.toggle(effects);
+        } else {
+            this.#handleDroppedItemWrapper.toggle(effects);
+        }
+    }
+
+    #givethEffect({ actor, data, source }: GiveEffectOptions, _userId: string): any {
+        /**
+         * https://github.com/foundryvtt/pf2e/blob/1465f7190b2b8454094c50fa6d06e9902e0a3c41/src/module/actor/sheet/base.ts#L1224
+         */
+        if (source.type === "condition") {
+            const value = data.value;
+            if (typeof value === "number" && source.system.value.isValued) {
+                source.system.value.value = value;
+            }
+
+            return actor.increaseCondition(source.system.slug, { value });
+        }
+
+        /**
+         * https://github.com/foundryvtt/pf2e/blob/1465f7190b2b8454094c50fa6d06e9902e0a3c41/src/module/actor/sheet/base.ts#L1238
+         */
+        const { level, value, context } = data;
+        if (typeof level === "number" && level >= 0) {
+            source.system.level.value = Math.floor(level);
+        }
+        if (source.type === "effect" && source.system.badge?.type === "counter" && typeof value === "number") {
+            source.system.badge.value = value;
+        }
+        source.system.context = context ?? null;
+        const originItem = fromUuidSync(context?.origin.item ?? "");
+        if (source.system.traits?.value.length === 0 && isInstanceOf(originItem, "SpellPF2e")) {
+            const spellTraits: string[] = originItem.system.traits.value;
+            const effectTraits = spellTraits.filter((t): t is EffectTrait => t in CONFIG.PF2E.effectTraits);
+            source.system.traits.value.push(...effectTraits);
+        }
+
+        actor.sheet["_onDropItemCreate"](new Item.implementation(source).clone().toObject());
+    }
+
+    #shouldHandleEffectDrop(item: ItemPF2e, actor: ActorPF2e): item is EffectPF2e | ConditionPF2e {
+        return (
+            item.isOfType("condition", "effect") &&
+            !actor.isOwner &&
+            (this.settings.effect === "all" || isAllyActor(actor))
+        );
+    }
+
+    async #creatureSheetHandleDroppedItem(
+        actorSheet: CreatureSheetPF2e<CreaturePF2e>,
+        wrapped: libWrapper.RegisterCallback,
+        event: DragEvent,
+        originalItem: ItemPF2e,
+        data: DropCanvasItemData,
+    ): Promise<ItemPF2e[]> {
+        const actor = actorSheet.actor;
+
+        if (!this.#shouldHandleEffectDrop(originalItem, actor)) {
+            return wrapped(event, originalItem, data);
+        }
+
+        if (originalItem.isOfType("condition") || !originalItem.system.rules.some((rule) => rule.key === "ChoiceSet")) {
+            this.#givethEmitable.call({
+                actor,
+                data,
+                source: originalItem.toObject(),
+            });
+
+            return [originalItem];
+        }
+
+        const RuleCls = game.pf2e.RuleElements.builtin.ChoiceSet as typeof ChoiceSetRuleElement;
+        if (!RuleCls) return [originalItem];
+
+        const ItemCls = getDocumentClass("Item");
+        const item = new ItemCls(originalItem.toObject(), { parent: actor }) as ItemPF2e<CreaturePF2e>;
+        const itemSource = item._source as EffectSource | ConditionSource;
+        const itemUpdates: EmbeddedDocumentUpdateData[] = [];
+
+        for (const [sourceIndex, source] of item.system.rules.entries()) {
+            if (source.key !== "ChoiceSet") continue;
+
+            try {
+                const rule = new RuleCls(source, { parent: item, sourceIndex });
+                const ruleSource = itemSource.system.rules[sourceIndex];
+
+                await rule.preCreate({
+                    ruleSource,
+                    itemSource,
+                    tempItems: [],
+                    itemUpdates,
+                    operation: {},
+                    pendingItems: [],
+                });
+            } catch (error) {}
+        }
+
+        this.#givethEmitable.call({
+            actor,
+            data,
+            source: itemSource,
+        });
+
+        return [originalItem];
+    }
+}
+
+type GiveEffectOptions = {
+    actor: CreaturePF2e;
+    data: DropCanvasItemData;
+    source: EffectSource | ConditionSource;
+};
+
+type ToolSettings = {
+    effect: (typeof EFFECT_SETTING)[number];
+};
+
+export { GivethTool };
