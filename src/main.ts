6d6c0943a23e3ecdcc3a37fb2498fb3bb7788aa1
Idle <651462+reonZ@users.noreply.github.com>
2026-04-18 14:50:08 +0200

    increase tracker sidebar width

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ import {
 } from "tools";
 
 const TOOLS = [
-    actionable,
+    // actionable,
     anonymousTool,
     new ArpTool(),
     new AutoCoverTool(),
@@ -36,7 +36,7 @@ const TOOLS = [
     new BetterInventoryTool(),
     betterMerchantTool,
     new BetterMovementTool(),
-    new BetterTemplateTool(),
+    // new BetterTemplateTool(),
     new BetterTradeTool(),
     new CharacterImporterTool(),
     new ConditionManagerTool(),
@@ -47,10 +47,10 @@ const TOOLS = [
     new MergeDamageTool(),
     new ResourceTrackerTool(),
     new RollTrackerTool(),
-    new UnidedTool(),
-    shareDataTool,
-    targetHelperTool,
-    new UndergroundTool(),
+    // new UnidedTool(),
+    // shareDataTool,
+    // targetHelperTool,
+    // new UndergroundTool(),
 ] as const;
 
 MODULE.register("pf2e-toolbelt");
