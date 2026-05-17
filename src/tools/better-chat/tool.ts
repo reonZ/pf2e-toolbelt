1d37686c5766c00568175413927e61a02940566d
Idle <651462+reonZ@users.noreply.github.com>
2026-05-17 17:49:29 +0200

    add new damage-average feature

--- src/tools/better-chat/tool.ts
+++ src/tools/better-chat/tool.ts
@@ -2,21 +2,28 @@ import {
     ChatMessagePF2e,
     createToggleHook,
     DamageDamageContextFlag,
+    DamageRoll,
     refreshLatestMessages,
+    Rolled,
     RollJSON,
     SYSTEM,
+    toggleHooksAndWrappers,
     TokenDocumentUUID,
 } from "foundry-helpers";
 import { ModuleTool, ToolSettingsList } from "module-tool";
 import { targetHelperTool } from "tools";
 import { injectDamage, mergeDamages, MergeOptions, renderChatMessageMergeDamage } from ".";
+import { htmlQuery } from "foundry-helpers/src";
+
+const AVERAGE_SETTING_TYPES = ["disabled", "standalone", "background"] as const;
 
 class BetterChatTool extends ModuleTool<ToolSettings> {
-    #renderChatMessageHook = createToggleHook("renderChatMessageHTML", this.#onRenderChatMessage.bind(this));
-    #diceSoNiceMessageProcessedHook = createToggleHook(
-        "renderChatMessageHTML",
-        this.#onDiceSoNiceMessageProcessed.bind(this),
-    );
+    #mergeDamageHooks = [
+        createToggleHook("renderChatMessageHTML", renderChatMessageMergeDamage.bind(this)),
+        createToggleHook("diceSoNiceMessageProcessed", this.#onDiceSoNiceMessageProcessed.bind(this)),
+    ];
+
+    #averageHook = createToggleHook("renderChatMessageHTML", this.#renderChatMessageAverageDamage.bind(this));
 
     get key(): "betterChat" {
         return "betterChat";
@@ -38,6 +45,16 @@ class BetterChatTool extends ModuleTool<ToolSettings> {
                 type: Boolean,
                 default: false,
                 scope: "user",
+                onChange: () => {
+                    this.configurate();
+                },
+            },
+            {
+                key: "average",
+                type: String,
+                default: "disabled",
+                scope: "user",
+                choices: AVERAGE_SETTING_TYPES,
                 onChange: () => {
                     this.configurate();
                 },
@@ -67,16 +84,12 @@ class BetterChatTool extends ModuleTool<ToolSettings> {
     }
 
     init(): void {
-        this._configurate();
+        this.configurate();
     }
 
     _configurate(): void {
-        const inject = this.settings.inject;
-        const merge = this.settings.merge;
-
-        this.#renderChatMessageHook.toggle(inject || merge);
-        this.#diceSoNiceMessageProcessedHook.toggle(inject || merge);
-
+        this.#averageHook.toggle(this.settings.average !== "disabled");
+        toggleHooksAndWrappers(this.#mergeDamageHooks, this.settings.merge || this.settings.inject);
         refreshLatestMessages(20);
     }
 
@@ -91,10 +104,38 @@ class BetterChatTool extends ModuleTool<ToolSettings> {
         return target ? [target] : [];
     }
 
-    async #onRenderChatMessage(message: ChatMessagePF2e, html: HTMLElement) {
-        if (this.settings.merge || this.settings.inject) {
-            renderChatMessageMergeDamage.call(this, message, html);
+    #renderChatMessageAverageDamage(message: ChatMessagePF2e, html: HTMLElement) {
+        if (!message.isDamageRoll) return;
+
+        const roll = message.rolls.find((roll): roll is Rolled<DamageRoll> => !roll.options.splashOnly);
+        if (!roll || (!roll.options.showBreakdown && !game.user.isGM)) return;
+
+        const minValue = roll.minimumValue;
+        const maxValue = roll.maximumValue;
+        if (minValue === maxValue) return;
+
+        const total = roll.total;
+        const ratio = (total - minValue) / (maxValue - minValue);
+        const hue = (1 - ratio) * 120;
+
+        if (this.settings.average === "background") {
+            const formulaElement = htmlQuery(html, ".message-content .dice-result .dice-formula");
+
+            formulaElement?.classList.add("show-average");
+            formulaElement?.style.setProperty("--average-ratio", `${ratio}`);
+            formulaElement?.style.setProperty("--average-hue", `${hue}`);
+
+            return;
         }
+
+        const diceRollElement = htmlQuery(html, ".dice-roll.damage-roll");
+
+        const template = `<div class="average-display" style="--average-ratio: ${ratio}; --average-hue: ${hue}">
+            <span>${minValue}</span>
+            <span>${maxValue}</span>
+        </div>`;
+
+        diceRollElement?.insertAdjacentHTML("beforebegin", template);
     }
 
     #onDiceSoNiceMessageProcessed(messageId: string, interception: { willTrigger3DRoll: boolean }) {
@@ -110,6 +151,7 @@ class BetterChatTool extends ModuleTool<ToolSettings> {
 }
 
 type ToolSettings = {
+    average: (typeof AVERAGE_SETTING_TYPES)[number];
     inject: boolean;
     merge: boolean;
 };
